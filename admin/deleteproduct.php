e85bf9f54596231868d6d6fb68e8b05425f6c8cb
Salonee Shrestha <saloneeshrestha35@gmail.com>
2024-09-12 20:45:53 +0545

    products added

--- admin/deleteproduct.php
+++ admin/deleteproduct.php
@@ -2,15 +2,15 @@
 require_once "./logincheck.php";
 
 if (!isset($_GET['id'])) {
-    header("Location: categories.php?error=No category found with the given ID.");
+    header("Location: products.php?error=No product found with the given ID.");
     die;
 }
 
 $id=(int) $_GET['id'];
 
-$sql="delete from `categories` where id=$id";
+$sql="delete from `products` where id=$id";
 $stmt=$con->prepare($sql);
 $stmt->execute();
 
-header("Location:categories.php?success=Selected category is deleted successfully.");
+header("Location:products.php?success=Selected product is deleted successfully.");
 die;
